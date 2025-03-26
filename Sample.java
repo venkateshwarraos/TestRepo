9cd6e80369a199ddd3255f18fd3438e0560abe2e
venkateshwar <venkyrao553@gmail.com>
2025-03-26 10:30:52 +0530

    added sample class

---
+++ Sample.java
@@ -0,0 +1,24 @@
+public class Sample {
+    private int instanceVariable =2;
+
+    public static void main(String[] args) {
+
+        int c = Test.add(5,6);
+
+        Sample sample = new Sample();
+        sample.getPrint();
+
+
+        System.out.println(c);
+
+
+
+    }
+
+    public void getPrint() {
+
+        System.out.println("sample print");
+
+
+    }
+}
--- Test.java
+++ Test.java
@@ -1,6 +1,19 @@
 public class Test {
 
+    private int i=0;
+
+
     public static void main(String[] args) {
-        System.out.println("this is test file new");
+        System.out.println("this is test file ");
+        add(2,3);
+
+
+
+    }
+
+    public static int  add(int i, int j) {
+
+
+        return i+j+10;
     }
 }
